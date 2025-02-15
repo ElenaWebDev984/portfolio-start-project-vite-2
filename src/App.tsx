8f7c6188a1981c750a6d433a947f8b1ca3680252
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-15 18:18:43 +0100

    add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import {Header} from "./layout/header/Header.tsx";
 
 
 function App() {
     return (
         <div className="App">
-
+            <Header />
         </div>
     )
 }
