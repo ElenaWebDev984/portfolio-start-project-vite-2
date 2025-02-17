1ecca19f7e87e1f93788018931041a73fb05cab0
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-17 14:48:40 +0100

    add Icon

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -5,11 +5,12 @@ type IconPropsType = {
     width?: string
     height?: string
     viewBox?: string
+    overflow?: string
 }
 
 export const Icon = (props: IconPropsType) => {
     return (
-        <svg height={props.height || '50'} width={props.width || '50'} viewBox={props.viewBox || '0 0 50 50'}
+        <svg height={props.height || '50'} width={props.width || '50'} viewBox={props.viewBox || '0 0 50 50'} overflow={props.overflow || 'visible'}
              xmlns="http://www.w3.org/2000/svg">
             <use xlinkHref={`${iconsSprite}#${props.iconId}`}/>
         </svg>
