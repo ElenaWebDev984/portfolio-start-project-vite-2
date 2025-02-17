cbfba13a5ae4c228feb146cb20f0ed1850509c74
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-17 14:47:39 +0100

    add Skill

---
+++ src/layout/sections/skills/skill/Skill.tsx
@@ -0,0 +1,18 @@
+import {Icon} from "../../../../components/icon/Icon.tsx";
+import styled from "styled-components";
+
+type SkillPropsType = {
+    iconId: string
+}
+
+export const Skill = (props: SkillPropsType) => {
+    return (
+        <StyledSkill>
+            <Icon iconId={props.iconId} />
+        </StyledSkill>
+    );
+};
+
+const StyledSkill = styled.div`
+    
+`
