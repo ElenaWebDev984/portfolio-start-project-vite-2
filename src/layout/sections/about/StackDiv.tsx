ddce602a91dfad129af55f45524e04c682cd5e21
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-19 20:44:13 +0100

    add StackDiv

---
+++ src/layout/sections/about/StackDiv.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import {Icon} from "../../../components/icon/Icon.tsx";
+
+type StackDivPropsType = {
+    iconId: string
+    text: string
+}
+
+export const StackDiv = (props: StackDivPropsType) => {
+    return (
+        <StyledStackDiv>
+            <Icon iconId={props.iconId} />
+            <AboutIconSpan>{props.text}</AboutIconSpan>
+        </StyledStackDiv>
+    );
+};
+
+
+const StyledStackDiv = styled.div`
+    display: flex;
+    text-align: center;
+`
+
+
+const AboutIconSpan = styled.span`
+
+`
