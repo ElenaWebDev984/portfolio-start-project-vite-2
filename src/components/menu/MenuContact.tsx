7440790fea74bfe67ead93ceb4e3fff49e57ca34
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-19 12:45:40 +0100

    add MenuContact

---
+++ src/components/menu/MenuContact.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import {Icon} from "../icon/Icon.tsx";
+
+
+export const MenuContact = () => {
+    return (
+        <StyledMenuContact>
+            <ul>
+                <li>
+                    <a href="">
+                        <Icon iconId={'github-cont'} />
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <Icon iconId={'twitter-cont'} />
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <Icon iconId={'linkedin-cont'} />
+                    </a>
+                </li>
+            </ul>
+        </StyledMenuContact>
+    );
+};
+
+const StyledMenuContact = styled.nav`
+    ul {
+        display: flex;
+        list-style: none;
+    }
+`
