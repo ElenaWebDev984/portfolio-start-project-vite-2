153a5d3710fda19e7e2857fb0fc62ba00041f79d
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-19 12:47:45 +0100

    add MenuContact

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,13 +1,18 @@
 import styled from "styled-components";
 import {Logo} from "../../components/logo/Logo.tsx";
 import {Menu} from "../../components/menu/Menu.tsx";
+import {MenuContact} from "../../components/menu/MenuContact.tsx";
+import {FlexWrapper} from "../../components/FlexWrapper.tsx";
 
 
 export const Header = () => {
     return (
         <StyledHeader>
             <Logo />
-            <Menu />
+            <FlexWrapper>
+                <Menu />
+                <MenuContact />
+            </FlexWrapper>
         </StyledHeader>
     );
 };
