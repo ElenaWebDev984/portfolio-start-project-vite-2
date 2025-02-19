9fcf2006860cbc86de3074dc6b083456646dd99b
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-19 20:43:15 +0100

    add Stack

---
+++ src/layout/sections/about/Stack.tsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
+import {StackDiv} from "./StackDiv.tsx";
+import {StackDivLocationWrapper} from "./StackDivLocationWrapper.tsx";
+
+
+type StackPropsType = {
+    spanText: string
+    smallText: string
+    calendarText: string
+    cityLocText: string
+    locationText: string
+}
+
+
+export const Stack = (props: StackPropsType) => {
+    return (
+        <StyledStack>
+            <FlexWrapper justifyContent={'space-between'}>
+                <AboutSpan>{props.spanText}</AboutSpan>
+                <AboutSmall>{props.smallText}</AboutSmall>
+            </FlexWrapper>
+
+            <FlexWrapper justifyContent={'space-between'}>
+                <StackDivLocationWrapper cityLocText={props.cityLocText} locationText={props.locationText} />
+                <StackDiv iconId={'calendar'} text={props.calendarText} />
+            </FlexWrapper>
+        </StyledStack>
+    );
+};
+
+const StyledStack = styled.div`
+    
+`
+
+const AboutSpan = styled.span`
+
+`
+
+const AboutSmall = styled.small`
+
+`
+
