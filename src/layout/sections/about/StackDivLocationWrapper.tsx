16506f24b1ce3fb40b355bd96a6ac3a3c8841ab1
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-19 20:44:35 +0100

    add StackDivLocationWrapper

---
+++ src/layout/sections/about/StackDivLocationWrapper.tsx
@@ -0,0 +1,22 @@
+import {StackDiv} from "./StackDiv.tsx";
+import styled from "styled-components";
+
+type StackDivLocationWrapperPropsType = {
+    cityLocText: string
+    locationText: string
+}
+
+
+export const StackDivLocationWrapper = (props: StackDivLocationWrapperPropsType) => {
+    return (
+        <StyledStackDivLocation>
+            <StackDiv iconId={'city-location'} text={props.cityLocText} />
+            <StackDiv iconId={'location'} text={props.locationText} />
+        </StyledStackDivLocation>
+    );
+};
+
+
+const StyledStackDivLocation = styled.div`
+    display: flex;
+`
