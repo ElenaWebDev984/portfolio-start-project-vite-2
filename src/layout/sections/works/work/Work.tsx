4006161e82ca002d1049441e357d90977565bcc4
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-20 17:25:27 +0100

    add Work

---
+++ src/layout/sections/works/work/Work.tsx
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+import {MenuProject} from "./MenuProject.tsx";
+import {FlexWrapper} from "../../../../components/FlexWrapper.tsx";
+import {ProjectLinkDiv} from "./ProjectLinkDiv.tsx";
+
+
+const projectItems = ['HTML, ', 'JavaScript, ', 'SASS, ', 'React' ,]
+
+type WorkPropsType = {
+    title: string
+    text: string
+    src: string
+    alt: string
+}
+
+
+export const Work = (props: WorkPropsType) => {
+    return (
+        <StyledWork>
+            <Image src={props.src} alt={props.alt} />
+            <WorkTitle>{props.title}</WorkTitle>
+            <WorkText>{props.text}</WorkText>
+            <FlexWrapper>
+                <span>Tech stack :</span>
+                <MenuProject menuItems={projectItems} />
+            </FlexWrapper>
+            <FlexWrapper gap={'20px'}>
+                <ProjectLinkDiv iconId={'proj-chain'} textLink={'Live Preview'} href={'#'}/>
+                <ProjectLinkDiv iconId={'proj-github'} textLink={'View Code'} href={'#'}/>
+            </FlexWrapper>
+        </StyledWork>
+    );
+};
+
+
+const StyledWork = styled.div`
+    background-color: #FFFFFF;
+    max-width: 375px;
+    width: 100%;
+`
+
+
+const Image = styled.img`
+    width: 100%;
+    height: 260px;
+    object-fit: cover;
+`
+
+
+const WorkTitle = styled.h3`
+
+`
+
+
+const WorkText = styled.p`
+
+`
+
