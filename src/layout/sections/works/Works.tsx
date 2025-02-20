71b9ac41c9c88985e6fdc4f8a0503a9087e36bbd
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-20 17:26:53 +0100

    add WorkSection

---
+++ src/layout/sections/works/Works.tsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+import {SectionTitle} from "../../../components/SectionTitle.tsx";
+import {SectionText} from "../../../components/SectionText.tsx";
+import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
+import {Work} from "./work/Work.tsx";
+import projectImg1 from './../../../assets/images/proj1.webp';
+import projectImg2 from './../../../assets/images/proj2.webp';
+import projectImg3 from './../../../assets/images/proj3.webp';
+import projectImg4 from './../../../assets/images/proj4.webp';
+import projectImg5 from './../../../assets/images/proj5.webp';
+import projectImg6 from './../../../assets/images/proj6.webp';
+
+
+export const Works = () => {
+    return (
+        <StyledWorks>
+            <SectionTitle>Projects</SectionTitle>
+            <SectionText>Things I’ve built so far</SectionText>
+            <FlexWrapper justifyContent={'space-around'} wrap={'wrap'} gap={'34px'} >
+                <Work title={'Project Tile goes here'} text={'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'} src={projectImg1} alt={'project1'} />
+                <Work title={'Project Tile goes here'} text={'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'} src={projectImg2} alt={'project2'} />
+                <Work title={'Project Tile goes here'} text={'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'} src={projectImg3} alt={'project3'} />
+                <Work title={'Project Tile goes here'} text={'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'} src={projectImg4} alt={'project4'} />
+                <Work title={'Project Tile goes here'} text={'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'} src={projectImg5} alt={'project5'} />
+                <Work title={'Project Tile goes here'} text={'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'} src={projectImg6} alt={'project6'} />
+            </FlexWrapper>
+        </StyledWorks>
+    );
+};
+
+const StyledWorks = styled.section`
+    min-height: 100vh;
+    background-color: #eaeeec;
+    border: 1px solid red;
+`
+
