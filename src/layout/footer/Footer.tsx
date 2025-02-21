696eb4f18c8c9f7438f60339dbd0534043c16504
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-21 18:04:50 +0100

    add Footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -11,13 +11,15 @@ const navItems = ['Home', 'About', 'Tech Stack', 'Projects', 'Contact',]
 export const Footer = () => {
     return (
         <StyledFooter>
-            <FlexWrapper>
+            <FlexWrapper justifyContent={'space-between'}>
                 <Logo />
-                <StackDiv iconId={'whatsapp'} text={'+91 12345 09876'} />
-                <StackDiv iconId={'gmail'} text={'info@example.com'} />
-                <MenuContact />
+                <FlexWrapper>
+                    <StackDiv iconId={'whatsapp'} text={'+91 12345 09876'} width={'30'} height={'30'} viewBox={'0 0 30 30'} />
+                    <StackDiv iconId={'gmail'} text={'info@example.com'} width={'30'} height={'30'} viewBox={'0 0 30 30'} />
+                    <MenuContact />
+                </FlexWrapper>
             </FlexWrapper>
-            <FlexWrapper>
+            <FlexWrapper justifyContent={'space-between'}>
                 <Menu menuItems={navItems} />
                 <StyledFooterText>Designed and built by Elena with Love & Coffee</StyledFooterText>
             </FlexWrapper>
