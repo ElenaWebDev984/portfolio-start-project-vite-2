eaf47c3f35e85be8551e970ed173034c19c109a1
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-23 22:05:23 +0100

    add styles for SectionTitle

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,6 +1,11 @@
 import styled from "styled-components";
+import {theme} from "../styles/Theme.tsx";
 
 export const SectionTitle = styled.h2`
-    color: #42446E;
+    color: ${theme.colors.fonsPrimary.textH2H3};
+    font-weight: 700;
+    font-size: 48px;
+    line-height: 26px;
     text-align: center;
+
 `
