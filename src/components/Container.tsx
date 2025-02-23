c56f2c2e9b3a07a929a21dbb82a49a624e0bada2
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-23 21:56:28 +0100

    add types

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+
+type ContainerPropsType = {
+    display?: string
+}
+
+export const Container = styled.div<ContainerPropsType>`
     max-width: 1230px;
     width: 100%;
     min-height: 100%;
