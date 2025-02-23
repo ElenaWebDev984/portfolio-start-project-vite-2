cdce173096ee2b3252a7fe052d75a7f172709c03
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-23 21:53:27 +0100

    add colores for SmallAbout

--- src/styles/Theme.tsx
+++ src/styles/Theme.tsx
@@ -1,19 +1,38 @@
-export const Theme = {
+export const theme = {
     colors: {
+        testBgc: '#eaeeec',
         primaryBgc: '#FFFFFF',
         secondaryBgc: '#191919',
-        // accent:
-        // linearGradientPic (180deg, #E70FAA 0%, #00C0FD 100%),
-        // linerGradientText background: (90deg, #13B0F5 -2.06%, #E70FAA 100%);
+        smallAboutBgc: '#D7FFE0',
+
+        accentPic: '#E70FAA',
+        accentPic2: '#00C0FD',
+        accentText: '#13B0F5',
+        accentText2: '#E70FAA',
+        // TODO delete accent colors after to do gradient
+        linearGradientPic: 'linear-gradient(180deg, #E70FAA 0%, #00C0FD 100%)',
+        linearGradientText: 'linear-gradient(90deg, #13B0F5 -2.06%, #E70FAA 100%)',
+        // TODO add GradientColors
+        // gradient: {
+        //     linearGradientText: {
+        //         background: 'linear-gradient(180deg, #E70FAA 0%, #00C0FD 100%)',
+        //         webkitBackgroundClip: 'text',
+        //         webkitTextFillColor: 'transparent',
+        //         backgroundClip: 'text',
+        //         color: 'transparent',
+        //         display: 'inline-block',
+        //     },
+        // },
         fonsPrimary: {
             textSection: '#666666', // linkNavHeader, text in Sections, textLoveFooter
-            textH2H3: '#42446E', // Titles in Sections, telef, email, linkNavFooter
+            textH2H3: '#42446E', // Titles in Sections, tel, email, linkNavFooter
             textAboutLink: '#A7A7A7', // sectionAbout - text+☸️
             textProjectTitle: '#000000', // ProjTitle, ProjLink
             textSlogan: '#1E0E62', // Slogan
+            textAboutSmall: '#018C0F',
         },
         fonsSecondary: {
-            textLinkNav: '#A7A7A7', // LinkNavHeader and Footer, textSkills, textPrpj, tel, email, textLoveFooter
+            textLinkNav: '#A7A7A7', // LinkNavHeader and Footer, textSkills, textProj, tel, email, textLoveFooter
             textSection: '#D9D9D9', // sectionMain
             textH2H3: '#CCCCCC', // Titles in Sections, titleProject, textProject, textSlogan
             textProjectLink: '#FFFFFF',
