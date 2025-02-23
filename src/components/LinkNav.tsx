88faf9cd14b3249e7126a6fc6c4cc1d0f8cab303
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-23 22:02:18 +0100

    add props.color for LinkNav

--- src/components/LinkNav.tsx
+++ src/components/LinkNav.tsx
@@ -4,14 +4,12 @@ import styled from "styled-components";
 type LinkNavPropsType = {
     fontWeight?: string
     fontSize?: string
-    color?: string
 }
 
 export const LinkNav = styled.a<LinkNavPropsType>`
     font-family: 'DM Sans', sans-serif;
     font-weight: ${props => props.fontWeight || '500'};
     font-size: ${props => props.fontSize || '20px'};
-    line-height: 26px;
+    //line-height: 26px;
     text-align: center;
-    color: ${props => props.color || '#666666'};
 `
