df3b2eeb894f2c04ba7bf71cd14e44ece529e5eb
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-23 22:07:34 +0100

    add styles for AboutSpan, AboutSmall and ::beforeSmall

--- src/layout/sections/about/Stack.tsx
+++ src/layout/sections/about/Stack.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
 import {StackDiv} from "./StackDiv.tsx";
 import {StackDivLocationWrapper} from "./StackDivLocationWrapper.tsx";
+import {theme} from "../../../styles/Theme.tsx";
 
 
 type StackPropsType = {
@@ -34,10 +35,38 @@ const StyledStack = styled.div`
 `
 
 const AboutSpan = styled.span`
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 28px;
+    letter-spacing: 1px;
 
 `
 
 const AboutSmall = styled.small`
+    color: ${theme.colors.fonsPrimary.textAboutSmall};
+    font-weight: 600;
+    font-size: 9px;
+    line-height: 26px;
+    text-align: center;
+    position: relative;
+    z-index: 0;
+    
+    &::before {
+        content: '';
+        display: inline-block;
+        min-width: 100%;
+        //width: 84px;
+        height: 24px;
+        border-radius: 100px;
+        background: ${theme.colors.smallAboutBgc};
 
+        position: absolute;
+        //top: -3px;
+        //left: 612px;
+        bottom: 0;
+        z-index: -1;
+        //right: 50%;
+        //left: 50%;
+    }
 `
 
