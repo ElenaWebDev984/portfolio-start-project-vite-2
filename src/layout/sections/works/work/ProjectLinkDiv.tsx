0b2134e7006a3c7313cb121e3adfcc77cd94740f
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-25 21:35:23 +0100

    add gap

--- src/layout/sections/works/work/ProjectLinkDiv.tsx
+++ src/layout/sections/works/work/ProjectLinkDiv.tsx
@@ -24,6 +24,7 @@ export const ProjectLinkDiv = (props: ProjectLinkDivPropsType) => {
 const StyledProjectLinkDiv = styled.div`
     display: flex;
     border: 1px solid violet;
+    gap: 3px;
 `
 
 
