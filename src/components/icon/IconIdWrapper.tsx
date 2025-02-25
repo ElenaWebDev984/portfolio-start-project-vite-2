19228daa56fe4041d797249c3cc9095912af5916
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-25 17:09:03 +0100

    create IconIdWrapper

---
+++ src/components/icon/IconIdWrapper.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+
+type IconIdWrapper = {
+    width?: string
+    height?: string
+    justifyContent?: string
+    alignItems?: string
+    gap?: string
+}
+
+export const IconIdWrapper = styled.div<IconIdWrapper>`
+    display: flex;
+    width: ${props => props.width  || '20px'};
+    height: ${props => props.height  || '20px'};
+    justify-content: ${props => props.justifyContent || 'center'};
+    align-items: ${props => props.alignItems || 'center'};
+    gap: ${props => props.gap};
+`
+
