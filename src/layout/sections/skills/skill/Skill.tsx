6462a53e75ab6bf2e11216a09a68d94227ab355e
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-25 17:10:19 +0100

    add IconIdWrapper

--- src/layout/sections/skills/skill/Skill.tsx
+++ src/layout/sections/skills/skill/Skill.tsx
@@ -1,5 +1,6 @@
 import {Icon} from "../../../../components/icon/Icon.tsx";
 import styled from "styled-components";
+import {IconIdWrapper} from "../../../../components/icon/IconIdWrapper.tsx";
 
 type SkillPropsType = {
     iconId: string
@@ -9,17 +10,20 @@ type SkillPropsType = {
     overflow?: string
 }
 
+
 export const Skill = (props: SkillPropsType) => {
     return (
         <StyledSkill>
-            <Icon iconId={props.iconId} />
+            <IconIdWrapper gap={'105px'} width={'120px'} height={'120px'} >
+                <Icon iconId={props.iconId} />
+            </IconIdWrapper>
         </StyledSkill>
     );
 };
 
 const StyledSkill = styled.div`
     width: 13%;
-    //background-color: beige;
     margin: 10px;
-    //position: relative;
 `
+
+
