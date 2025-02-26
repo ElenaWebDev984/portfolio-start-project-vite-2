657c4acc45fba102ab417e8f0e7f36551dbfbda1
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-26 02:02:51 +0100

    add styled for Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,12 @@
 import styled from "styled-components";
+import {theme} from "../styles/Theme.tsx";
 
 export const Button = styled.button`
-
+    width: 150px;
+    height: 40px;
+    font-weight: 600;
+    font-size: 20px;
+    cursor: pointer;
+    background-image: ${theme.colors.linearGradientPic};
+    color: ${theme.colors.fonsPrimary.textSlogan};
 `
