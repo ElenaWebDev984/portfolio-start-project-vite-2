ef94190ec28461b7c87ada57e1f73c379bb7c6bd
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-27 19:52:45 +0100

    add IconIdWrapper and ContactSloganSpan

--- src/layout/sections/slogan/Slogan.tsx
+++ src/layout/sections/slogan/Slogan.tsx
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
-import {StackDiv} from "../about/StackDiv.tsx";
 import {Container} from "../../../components/Container.tsx";
+import {theme} from "../../../styles/Theme.tsx";
+import {Icon} from "../../../components/icon/Icon.tsx";
+import {IconIdWrapper} from "../../../components/icon/IconIdWrapper.tsx";
 
 
 export const Slogan = () => {
@@ -9,8 +11,13 @@ export const Slogan = () => {
         <StyledSlogan>
             <Container>
                 <FlexWrapper direction="column" justifyContent={'center'} alignItems={'center'}>
-                    <StyledSloganText>More projects I’ve worked on</StyledSloganText>
-                    <StackDiv iconId={'github-cont'} text={'@john-doe on github'} />
+                    <StyledSloganText>More projects I’ve worked on:</StyledSloganText>
+                    <FlexWrapper alignItems={'center'}>
+                        <IconIdWrapper >
+                            <Icon iconId={'github-cont'} />
+                        </IconIdWrapper>
+                        <ContactSloganSpan>@john-doe on github</ContactSloganSpan>
+                    </FlexWrapper>
                 </FlexWrapper>
             </Container>
         </StyledSlogan>
@@ -18,11 +25,25 @@ export const Slogan = () => {
 };
 
 const StyledSlogan = styled.section`
-   
+    font-family: 'DM Sans', sans-serif;
+    font-weight: 700;
+    font-size: 58px;
+    line-height: 70px;
+    letter-spacing: -1px;
 `
 
 
 const StyledSloganText = styled.p`
+    color: ${theme.colors.fonsPrimary.textSlogan};
+`
 
+const ContactSloganSpan = styled.span`
+    background: ${theme.colors.linearGradientText};
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-clip: text;
+    color: transparent;
+    display: inline-block;
+    margin-top: 10px;
 `
 
