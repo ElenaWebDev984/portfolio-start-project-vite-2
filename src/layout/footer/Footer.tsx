9953011247e1023a17c7d1b22946c8bdd46d2447
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-27 19:50:30 +0100

    add DivPseudoWrapper and ::beforeLine

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -5,6 +5,7 @@ import {Menu} from "../../components/menu/Menu.tsx";
 import {MenuContact} from "../../components/menu/MenuContact.tsx";
 import {StackDiv} from "../sections/about/StackDiv.tsx";
 import {Container} from "../../components/Container.tsx";
+import {theme} from "../../styles/Theme.tsx";
 
 
 const navItems = ['Home', 'About', 'Tech Stack', 'Projects', 'Contact',]
@@ -15,17 +16,18 @@ export const Footer = () => {
             <Container>
                 <FlexWrapper justifyContent={'space-between'}>
                     <Logo />
-                    <FlexWrapper>
+                    <FlexWrapper justifyContent={'flex-end'} gap={'60px'}>
                         <StackDiv iconId={'whatsapp'} text={'+91 12345 09876'} width={'30'} height={'30'} viewBox={'0 0 30 30'} />
                         <StackDiv iconId={'gmail'} text={'info@example.com'} width={'30'} height={'30'} viewBox={'0 0 30 30'} />
                         {/*TODO change sizes for icons*/}
                         <MenuContact />
                     </FlexWrapper>
                 </FlexWrapper>
+                <DivPseudoWrapper />
                 <FlexWrapper justifyContent={'space-between'}>
                     <Menu menuItems={navItems} />
                     {/*TODO change sizes, color, etc. for links by PROPS*/}
-                    <StyledFooterText>Designed and built by Elena with Love & Coffee</StyledFooterText>
+                    <StyledFooterText>© 2025 Designed and built by <span>Elena</span> with <span>Love</span> & <span>Coffee</span></StyledFooterText>
                 </FlexWrapper>
             </Container>
         </StyledFooter>
@@ -34,10 +36,33 @@ export const Footer = () => {
 
 
 const StyledFooter = styled.footer`
-    
+    padding: 60px 0;
 `
 
 
 const StyledFooterText = styled.p`
+    font-weight: 400;
+    font-size: 18px;
 
+
+    span {
+        background: ${theme.colors.linearGradientText};
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        background-clip: text;
+        color: transparent;
+        display: inline-block;
+    }
+`
+
+const DivPseudoWrapper = styled.div`
+    &::before {
+        content: '';
+        display: inline-block;
+        width: 100%;
+        height: 2px;
+        background-color: ${theme.colors.fonsPrimary.textSection};
+        opacity: 30%;
+        margin: 45px 0;
+    }
 `
