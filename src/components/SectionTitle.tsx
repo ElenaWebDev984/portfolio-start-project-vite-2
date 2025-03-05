9ee96993e32b88158007655261f9f3d76912b9ec
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-05 15:02:11 +0100

    add MixIn

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 import {theme} from "../styles/Theme.tsx";
+import {font} from "../styles/Common.ts";
 
 export const SectionTitle = styled.h2`
+    ${font({weight: 700, Fmax: 48, Fmin: 32})};
     color: ${theme.colors.fonsPrimary.textH2H3};
-    font-weight: 700;
-    font-size: 48px;
+    //font-weight: 700;
+    //font-size: 48px;
     line-height: 26px;
     text-align: center;
     margin-bottom: 50px;
