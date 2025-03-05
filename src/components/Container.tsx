71b78e28475c553ef84b54e4c8f0f506f433bf94
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-05 15:00:03 +0100

    add outline

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -11,5 +11,5 @@ export const Container = styled.div<ContainerPropsType>`
     min-height: 100%;
     padding: 0 15px;
     margin: 0 auto;
-    //outline: 1px solid red;
+    outline: 1px solid red;
 `
