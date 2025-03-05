ecff543aef5230773937948acdad09bf49d7199d
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-05 15:01:59 +0100

    add MixIn

--- src/components/SectionText.tsx
+++ src/components/SectionText.tsx
@@ -1,8 +1,10 @@
 import styled from "styled-components";
+import {font} from "../styles/Common.ts";
 
 export const SectionText = styled.p`
-    font-weight: 400;
-    font-size: 32px;
+    ${font({weight: 400, Fmax: 32, Fmin: 24})};
+    //font-weight: 400;
+    //font-size: 32px;
     line-height: 26px;
     text-align: center;
     margin-bottom: 110px;
