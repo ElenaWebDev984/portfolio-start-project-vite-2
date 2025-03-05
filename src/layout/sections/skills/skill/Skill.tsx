e1d82c30651f1c051beb09c107e130ad70134bb7
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-03-05 15:02:37 +0100

    add @media

--- src/layout/sections/skills/skill/Skill.tsx
+++ src/layout/sections/skills/skill/Skill.tsx
@@ -1,6 +1,7 @@
 import {Icon} from "../../../../components/icon/Icon.tsx";
 import styled from "styled-components";
 import {IconIdWrapper} from "../../../../components/icon/IconIdWrapper.tsx";
+import {theme} from "../../../../styles/Theme.tsx";
 
 type SkillPropsType = {
     iconId: string
@@ -14,7 +15,7 @@ type SkillPropsType = {
 export const Skill = (props: SkillPropsType) => {
     return (
         <StyledSkill>
-            <IconIdWrapper gap={'105px'} width={'120px'} height={'120px'} >
+            <IconIdWrapper gap={'105px'} width={'120px'} height={'120px'} justifyContent={'space-between'}>
                 <Icon iconId={props.iconId} />
             </IconIdWrapper>
         </StyledSkill>
@@ -24,6 +25,16 @@ export const Skill = (props: SkillPropsType) => {
 const StyledSkill = styled.div`
     width: 13%;
     margin: 10px;
+    flex-grow: 1;
+
+    @media ${theme.media.tablet} {
+       width: 18%;
+       
+    }
+
+    @media ${theme.media.mobile} {
+        width: 20%;
+    }
 `
 
 
